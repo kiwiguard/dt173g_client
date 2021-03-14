cc1e3109eeac8f941670c2e4d763eacb3bc38157
Susanne Nilsson <45233211+kiwiguard@users.noreply.github.com>
2021-03-14 18:58:41 +0100

    added full design and functionality for desktop

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,24 @@
 // Mobile-menu scripts
+
+function openNav() {
+    var openMenu = document.getElementById('open-menu');
+    var links = document.getElementById('nav-links');
+    var closeMenu = document.getElementById('close-menu');
+    var body = document.getElementById('body');
+    openMenu.style.padding = '1rem 1rem 200vw 200vw';
+    links.style.display = 'flex';
+    closeMenu.style.display = 'inline';
+    body.classList.add('stop-scrolling');
+}
+
+function closeNav() {
+    var openMenu = document.getElementById('open-menu');
+    var links = document.getElementById('nav-links');
+    var closeMenu = document.getElementById('close-menu');
+    var body = document.getElementById('body');
+    openMenu.style.padding = '1rem 1rem 2rem 2rem';
+    openMenu.style.display = 'inline';
+    links.style.display = 'none';
+    closeMenu.style.display = 'none';
+    body.classList.remove('stop-scrolling');
+}
